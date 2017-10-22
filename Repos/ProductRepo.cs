51c09be62234945fe0d887ae160ee4e213e86fab
Sean J. Gallen <sean.j.gallen@avanade.com>
2017-10-21 21:13:35 -0500

    Started to use part of the repo

---
+++ Repos/ProductRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using server.Models;
+using server.Data;
+
+namespace server.Repos
+{
+    public class ProductRepo<T> : IProductRepo<T> where T : BaseEntity
+    {
+        /// <summary>
+        /// Product Repo Pattern
+        /// </summary>
+        /// 
+        /// <typeparam name="T"></typeparam>
+
+        private OrderContext _orderContext;
+
+        public ProductRepo(OrderContext orderContext)
+        {
+            _orderContext = orderContext;
+        }
+
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
