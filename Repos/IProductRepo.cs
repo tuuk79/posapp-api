bddf301253a310c269db2d649a138d1fb0dbe8b1
Sean J. Gallen <sean.j.gallen@avanade.com>
2017-10-21 20:37:14 -0500

    added Repo Interface

---
+++ Repos/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using server.Models;
+using System.Collections.Generic;
+
+namespace server.Repos
+{
+    interface IProductRepo<T> where T : BaseEntity
+    {
+        T GetById(int id);
+        IEnumerable<T> ListAll();
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
